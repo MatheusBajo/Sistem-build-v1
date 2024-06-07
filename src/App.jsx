b9ef491ce790b241e17e731ee7e6e9b283541a5d
MatheusBajo <96698464+MatheusBajo@users.noreply.github.com>
2024-06-06 22:13:34 -0300

    Primeiro Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <div className='bg-blue-400'>
+            <h1 className='font-bold'>Hello Tailwindcss, React and Vite!</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
